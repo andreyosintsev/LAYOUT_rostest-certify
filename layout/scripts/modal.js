858d009f4b39176eeda444e049bde9b6b04e168f
andreyosintsev <kintaro_oe@inbox.ru>
2024-10-28 12:14:09 +0400

    fix: modal and gosty

--- layout/scripts/modal.js
+++ layout/scripts/modal.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlay = document.querySelector('.modal-overlay');
     const closeButton = document.querySelector('.modal__close');
 
-    if (closeButton) closeButton.addEventListener('click', () => closeModal(modal, overlay));
-    if (overlay) overlay.addEventListener('click', () => closeModal(modal, overlay));
+    if (closeButton) closeButton.addEventListener('click', () => closeModal(body, modal, overlay));
+    if (overlay) overlay.addEventListener('click', () => closeModal(body, modal, overlay));
 
     document.addEventListener('keydown', e => {
-        if (e.key === 'Escape') closeModal(modal, overlay);
+        if (e.key === 'Escape') closeModal(body, modal, overlay);
     })
 })
 
-function closeModal(modal, overlay) {
+function closeModal(body, modal, overlay) {
     if (modal) modal.classList.remove('modal_visible');
     if (overlay) overlay.classList.remove('modal-overlay_visible');
     if (body) body.classList.remove('no-scroll');
