347a159903efd2cf6bbf218d0c1316682d822390
andreyosintsev <kintaro_oe@inbox.ru>
2024-11-10 17:54:15 +0400

    add: all layouts done

--- layout/scripts/modal.js
+++ layout/scripts/modal.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
-    const modal = document.querySelector('.modal');
+    const modals = document.querySelectorAll('.modal');
     const overlay = document.querySelector('.modal-overlay');
-    const closeButton = document.querySelector('.modal__close');
+    const closeButtons = document.querySelectorAll('.modal__close');
 
-    if (closeButton) closeButton.addEventListener('click', () => closeModal(body, modal, overlay));
-    if (overlay) overlay.addEventListener('click', () => closeModal(body, modal, overlay));
+    closeButtons.forEach(closeButton =>
+        closeButton.addEventListener('click',
+            () => closeModal(body, closeButton.closest('.modal'), overlay))
+    );
+
+    if (overlay) overlay.addEventListener('click', () => {
+        modals.forEach(modal => closeModal(body, modal, overlay));
+    });
 
     document.addEventListener('keydown', e => {
-        if (e.key === 'Escape') closeModal(body, modal, overlay);
+        if (e.key === 'Escape')
+            modals.forEach(modal => closeModal(body, modal, overlay));
     })
 })
 
 function closeModal(body, modal, overlay) {
     if (modal) modal.classList.remove('modal_visible');
-    if (overlay) overlay.classList.remove('modal-overlay_visible');
-    if (body) body.classList.remove('no-scroll');
+
+    const visibleModals = document.querySelectorAll('.modal_visible');
+
+    if (visibleModals.length === 0) {
+        if (overlay) overlay.classList.remove('modal-overlay_visible');
+        if (body) body.classList.remove('no-scroll');
+    }
 }
